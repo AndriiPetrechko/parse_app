3c52a09786a88349c7fa8c83b73e97547bacfaea
AndriiPetrechko <petrechkoandrij@gmail.com>
2024-05-06 16:32:11 +0300

    Add new features. Get catalogue pdf-links and download pdf files

--- index.js
+++ index.js
@@ -1,38 +1,73 @@
 const puppeteer = require('puppeteer');
+const axios = require('axios');
 const fs = require('fs');
 
 const URL = 'https://www.tus.si/#s2';
 const FILE_NAME = 'catalogs.json';
+const FOLDER_NAME = 'downloaded';
 
-(async () => {
+async function parsePage(pageUrl){
   const browser = await puppeteer.launch({
     headless: true
   });
   const page = await browser.newPage();
-  await page.goto(URL);
+  await page.goto(pageUrl);
 
   const catalogsInfo = [];
 
   const domElements = await page.$$('.card-catalogue');
-  for (let element of domElements) {
-    const obj = {}
 
-    const catalogueName = await element.$('.hover > h3 > a');
-    obj.catalogueName = (await catalogueName.evaluate(el => el.textContent, element)).trim();
+  await Promise.all(
+    domElements.map(async (element, i) => {
+      const obj = {}
 
-    const link = await element.$('.hover > h3 > a');
-    obj.link = (await link.evaluate(el => el.href, element)).trim();
-    
-    const date = await element.$('p');
-    obj.date = (await date.evaluate(el => el.textContent, element)).trim();   
-    
-    catalogsInfo.push(obj);
-  }
+      const catalogueName = await element.$('.hover > h3 > a');
+      obj.catalogueName = (await catalogueName.evaluate(el => el.textContent, element)).trim();
 
-  console.log(catalogsInfo);
+      const link = await element.$('.hover > .zoom > figcaption > a.pdf');
+      obj.link = (await link.evaluate(el => el.href, element)).trim();
+
+      await downloadFile(obj.link, `${FOLDER_NAME}/${obj.catalogueName}_${i}.pdf`);      
+      
+      const date = await element.$('p');
+      obj.date = (await date.evaluate(el => el.textContent, element)).trim();
+      
+      catalogsInfo.push(obj);
+      return obj
+    })
+  )
   
-  fs.writeFileSync(FILE_NAME, JSON.stringify(catalogsInfo, null, 2));
-  console.log(`Catalogs information saved to file ${FILE_NAME}`);
+  fs.writeFileSync(`${FOLDER_NAME}/${FILE_NAME}`, JSON.stringify(catalogsInfo, null, 2));
+  console.log(`Catalogs information saved to ${FOLDER_NAME}/${FILE_NAME}`);
 
   await browser.close();
-})();
+};
+
+
+async function createFolder(folderName) {
+  fs.mkdir(folderName, (err) => {
+  if (err) {
+    console.error('Error creating folder', err);
+    return;
+  }
+  console.log(`Folder "${folderName}" created successfully`);
+});
+}
+
+async function downloadFile(fileUrl, filePath) {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: fileUrl,
+      responseType: 'arraybuffer' 
+    });
+
+    fs.writeFileSync(filePath, response.data); 
+    console.log(`File ${filePath} saved successfully`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+createFolder(FOLDER_NAME);
+parsePage(URL);
